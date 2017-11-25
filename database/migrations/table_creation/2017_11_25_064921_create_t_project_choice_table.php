264619a9be6e271b17d6e01a4794985a1f242281
SaoGuang <31481474+saoguang@users.noreply.github.com>
2017-11-25 18:56:07 +0800

    fix migration error
    迁移小错误。

--- database/migrations/table_creation/2017_11_25_064921_create_t_project_choice_table.php
+++ database/migrations/table_creation/2017_11_25_064921_create_t_project_choice_table.php
@@ -17,12 +17,12 @@ class CreateTProjectChoiceTable extends Migration
             $table->string('project_name', 80)->comment('课题名称');
             $table->string('project_type', 10)->comment('课题类型');
             $table->string('project_origin', 10)->comment('课题来源');
-            $table->text('require_for_student')->unllable()->comment('对学生的要求');
+            $table->text('require_for_student')->nullable()->comment('对学生的要求');
             $table->string('project_declaration_status', 10)->comment('课题申报状态');
             $table->string('project_choice_status', 10)->comment('课题被选状态');
-            $table->string('session_id', 10)->comment('界别ID');
-            $table->string('teacher_job_number', 20)->comment('出题指导老师');            //外键
-            $table->string('student_number', 30)->default(0)->comment('选题学生');         //外键
+            $table->string('session_id', 10)->comment('届别ID');
+            $table->string('teacher_job_number', 20)->comment('出题指导老师');               //外键
+            $table->string('student_number', 30)->default('0')->comment('选题学生');         //外键
 
             $table->string('creator', 20)->nullable();
             $table->string('updater', 20)->nullable();
