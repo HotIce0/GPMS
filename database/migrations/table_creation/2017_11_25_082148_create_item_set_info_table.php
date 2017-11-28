77bef8b6a5718017e7bc8fa5f3b967fc04cb340a
SaoGuang <31481474+saoguang@users.noreply.github.com>
2017-11-28 13:48:55 +0800

    fix the migration
    修复迁移的选项设置信息表的复合唯一索引

--- database/migrations/table_creation/2017_11_25_082148_create_item_set_info_table.php
+++ database/migrations/table_creation/2017_11_25_082148_create_item_set_info_table.php
@@ -15,9 +15,10 @@ class CreateItemSetInfoTable extends Migration
         Schema::create('t_item_set_info', function (Blueprint $table) {
             $table->increments('item_id')->comment('选项ID');
             $table->integer('item_no')->unsigned()->unique()->comment('选项编号');
-            $table->string('item_content_id', 10)->unique()->comment('选项内容ID');
+            $table->string('item_content_id', 10)->comment('选项内容ID');
             $table->string('item_content', 200)->comment('选项内容');
             $table->tinyInteger('sort_id')->unsigned()->comment('排序ID');
+            $table->unique(['item_no','item_content_id']);
 
             $table->string('creator', 20)->nullable();
             $table->string('updater', 20)->nullable();
