0c7cbc7cab14fa17d1ef59a56cb3c0ac61744f43
SaoGuang <31481474+saoguang@users.noreply.github.com>
2017-12-03 01:22:32 +0800

    Add select2

--- resources/views/teacher/projectChecklist/projectChecklist.blade.php
+++ resources/views/teacher/projectChecklist/projectChecklist.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.layoutSidebar')
 {{--By Sao Guang--}}
+
+@section('head')
+    <title></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!-- VENDOR CSS -->
+    <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/vendor/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/vendor/linearicons/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/vendor/chartist/css/chartist-custom.css')}}">
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+    <!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+    <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}">
+    <!-- ICONS -->
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('assets/img/favicon.png')}}">
+    <link href="{{asset('assets/vendor/select2/select2.min.css')}}" rel="stylesheet">
+@endsection
+
 @section('sidebar')
     @include('teacher.sidebar')
 @endsection
@@ -92,7 +113,7 @@
     <script src="{{asset('assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js')}}"></script>
     <script src="{{asset('assets/vendor/chartist/js/chartist.min.js')}}"></script>
     <script src="{{asset('assets/scripts/klorofil-common.js')}}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <script src="{{asset('assets/vendor/select2/select2.min.js')}}"></script>
     <!-- CLICK SUBMIT -->
     <script>
         $(document).ready(function(){
