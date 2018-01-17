7c509a8c29c8ace38bcecc05942e56f069770829
Cheirmin <34155913+Cheirmin@users.noreply.github.com>
2018-01-18 00:56:27 +0800

    进入界面
    在主页加了信息管理入口的链接

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -3,21 +3,21 @@
     <div class="sidebar-scroll">
         <nav>
             <ul class="nav">
-                <li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
-                <li><a href="elements.html" class=""><i class="lnr lnr-code"></i> <span>Elements</span></a></li>
-                <li><a href="charts.html" class=""><i class="lnr lnr-chart-bars"></i> <span>Charts</span></a></li>
-                <li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
-                <li><a href="notifications.html" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
                 <li>
-                    <a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Pages</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+                    <a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>信息管理</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
                     <div id="subPages" class="collapse ">
                         <ul class="nav">
-                            <li><a href="page-profile.html" class="">Profile</a></li>
-                            <li><a href="page-login.html" class="">Login</a></li>
-                            <li><a href="page-lockscreen.html" class="">Lockscreen</a></li>
+                            <li><a href="page-profile.html" class="">学院信息管理</a></li>
+                            <li><a href="page-login.html" class="">班级信息管理</a></li>
+                            <li><a href="page-lockscreen.html" class="">教研室信息管理</a></li>
                         </ul>
                     </div>
                 </li>
+                <li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>没用</span></a></li>
+                <li><a href="elements.html" class=""><i class="lnr lnr-code"></i> <span>这个下面的</span></a></li>
+                <li><a href="charts.html" class=""><i class="lnr lnr-chart-bars"></i> <span>按钮</span></a></li>
+                <li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
+                <li><a href="notifications.html" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
                 <li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
                 <li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
                 <li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
