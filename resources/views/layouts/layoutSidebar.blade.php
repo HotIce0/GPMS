f030fce34b1b56cce0bb7312f37e42b27068f605
SaoGuang <31481474+saoguang@users.noreply.github.com>
2018-01-27 17:30:41 +0800

    fix errors

--- resources/views/layouts/layoutSidebar.blade.php
+++ resources/views/layouts/layoutSidebar.blade.php
@@ -23,28 +23,6 @@
         <div class="main-content">
             {{--<div class="container-fluid">--}}
 
-            @if (session('success'))
-                <div class="alert alert-success alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    {{ session('success') }}
-                </div>
-            @endif
-
-            @if (count($errors) > 0)
-                <div class="alert alert-danger alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
             @yield('content')
             {{--</div>--}}
         </div>
